155168b1b104c6a07ca1c839b30b3558ef079101
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-03 17:30:27 +0200

    exercice

---
+++ src/ExIfGreetingLanguageUx.java
@@ -0,0 +1,29 @@
+public class ExIfGreetingLanguageUx {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Vous devez introduire au minimum deux arguments");
+            System.out.println("1. la langue avec le choix suivant EN FR NL");
+            System.out.println("2. le nom de la personne à saluer");
+            return;
+        }
+
+        if (! args[0].equals("EN") && ! args[0].equals("FR") && ! args[0].equals("NL")) {
+            System.out.println("Le programme ne supporte que les langues EN FR NL");
+            return;
+        }
+
+        if (args[0].equals("EN")) {
+            System.out.print("Hello ");
+        }
+        if (args[0].equals("FR")) {
+            System.out.print("Bonjour ");
+        }
+        if(args[0].equals("NL")) {
+            System.out.print("Hallo ");
+        }
+        for (int i = 1;i < args.length; ++i) {
+            System.out.print(args[i] + " ");
+        }
+        System.out.println();
+    }
+}
