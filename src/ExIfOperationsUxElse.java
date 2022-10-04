6a0de16f7b433a99ae8db2079badeb2634197ed3
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-04 09:51:36 +0200

    exercice

---
+++ src/ExIfOperationsUxElse.java
@@ -0,0 +1,28 @@
+public class ExIfOperationsUxElse {
+    public static void main(String[] args) {
+        if (args.length == 3) {
+            if (args[1].equals("+") || args[1].equals("-") || args[1].equals("x") || args[1].equals("/")) {
+                System.out.print(args[0] + " " + args[1] + " " + args[2] + " = ");
+                if (args[1].equals("+")) {
+                    System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[2]));
+                }
+                if (args[1].equals("-")) {
+                    System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[2]));
+                }
+                if (args[1].equals("x")) {
+                    System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[2]));
+                }
+                if (args[1].equals("/")) {
+                    System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[2]));
+                }
+            } else {
+                System.out.println("Utilisez les caractères + - x / pour indiquer l'opération de votre choix");
+            }
+        } else {
+            System.out.println("Le programme prend trois arguments");
+            System.out.println("Le premier est un nombre entier");
+            System.out.println("Le second est + - x ou /");
+            System.out.println("Le troisième est un nombre entier");
+        }
+    }
+}
