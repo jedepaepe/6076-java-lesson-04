55c35cc6c970ba380d9df8a6ae769c85aed2a803
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-04 09:20:49 +0200

    exercice

---
+++ src/ExIfIsChildElse.java
@@ -0,0 +1,9 @@
+public class ExIfIsChildElse {
+    public static void main(String[] args) {
+        if (Integer.parseInt(args[0]) < 18) {
+            System.out.println("Tu es un enfant");
+        } else {
+            System.out.println("Tu es un adulte");
+        }
+    }
+}
